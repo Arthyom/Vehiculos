4024ac8096f7b14d5bdb6f978dfff32810ce89f2
alfredo.gonzalez@vientodigital.com <alfredo.gonzalez@vientodigital.com>
2025-04-01 22:29:22 -0600

    Adding inertia

--- app/Http/Controllers/PolloLoginController.php
+++ app/Http/Controllers/PolloLoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Auth;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
 use Session;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -14,7 +15,10 @@ class PolloLoginController extends Controller
 {
     //
     public function index() {
-        return view('Loggin.index');
+        // return view('Loggin.index');
+
+        // $user = User::find(1);
+        return inertia('Home');
     }
 
     public function login(Request $request)  {
